06e10546f0505c4bcbefe1a0d4ca45e568d0f921
Ahmed Mohamed <73834838+AhmedMohamedAbdelaty@users.noreply.github.com>
2023-04-05 02:42:44 +0200

    Update friends-standing.js

--- friends-standing.js
+++ friends-standing.js
@@ -7,24 +7,46 @@
 // @icon https://www.google.com/s2/favicons?sz=64&domain=codeforces.com
 // @match https://codeforces.com/*/problem/*
 // @match https://codeforces.com/contest/*
-// @match https://codeforces.com/gyms/*
+// @match https://codeforces.com/gym/*
 // @grant none
 // ==/UserScript==
 
 (function() {
     'use strict';
+// Get the contest ID from the URL
+let url = window.location;
+let contestId = url.toString().split("/").filter( (x)=> {
+    if(typeof x !== 'string') {return;}
+    const num = Number(x);
+    if(Number.isInteger(num)) {return num;}
+}) [0];
 
-    // Get the contest ID from the URL
-    let url = window.location;
-    let contestId = url.toString().split("/").filter( (x)=> {
-        if(typeof x !== 'string') {return;}
-        const num = Number(x);
-        if(Number.isInteger(num)) {return num;}
-    }) [0];
+// Check if the current URL is a gym or a regular contest
+let isGym = /gym/.test(url);
 
-    // Select the standings button element
-    let standingsBtn = document.querySelector(".second-level-menu-list > li:nth-child(6) > a");
+// Select the list of buttons
+let buttonsList = document.querySelector(".second-level-menu-list");
 
-    // Change its href attribute to point to the friends standings URL
-    standingsBtn.setAttribute("href", `https://codeforces.com/contest/${contestId}/standings/friends/true`);
+// Initialize a variable to store the standings button element
+let standingsBtn = null;
+
+// Loop over the list items and compare their text content with "Standings"
+for (let i = 0; i < buttonsList.children.length; i++) {
+  let button = buttonsList.children[i].querySelector("a");
+  if (button.textContent.includes("Standings")) {
+    // Found the standings button, assign it to the variable and break the loop
+    standingsBtn = button;
+    break;
+  }
+}
+
+// Check if the standings button was found
+if (standingsBtn) {
+  // Change its href attribute to point to the friends standings URL
+  // Use a ternary operator to assign the appropriate URL based on isGym
+  standingsBtn.setAttribute("href", isGym ? `https://codeforces.com/gym/${contestId}/standings/friends/true` : `https://codeforces.com/contest/${contestId}/standings/friends/true`);
+} else {
+  // The standings button was not found, handle the error
+  console.error("Could not find the standings button");
+}
 })();
